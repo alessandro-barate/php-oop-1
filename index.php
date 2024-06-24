206df83890d9c3a6a9be4eec1ac878424019b279
alessandro-barate <alebarat@gmail.com>
2024-06-24 15:57:07 +0100

    [UPD] Working on the onstructor

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 class Movie
 {
     public $title;
-    public  $genre;
+    public  $country;
+    // public $duration = getDuration();
+
+    function __construct($duration)
+    {
+    }
 }
 
-$trainspotting  = new Movie();
+$trainspotting = new Movie();    // tra () quello da passare al costruttore
 $trainspotting->title = "Trainspotting";
-$trainspotting->genre = "comedy";
+$trainspotting->country = "Scotland";
+
+$memento = new Movie(); // tra () quello da passare al costruttore
+$memento->title = "Memento";
+$memento->country = "United States";
