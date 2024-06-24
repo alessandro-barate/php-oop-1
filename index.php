9defc31fe9b72d7b259431d54c0ac8d07997d6bd
alessandro-barate <alebarat@gmail.com>
2024-06-24 16:07:26 +0100

    [UPD]

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ class Movie
     public  $country;
     // public $duration = getDuration();
 
-    function __construct($duration)
-    {
-    }
+    // function __construct($duration)
+    // {
+    // }
 }
 
 $trainspotting = new Movie();    // tra () quello da passare al costruttore
@@ -17,3 +17,6 @@ $trainspotting->country = "Scotland";
 $memento = new Movie(); // tra () quello da passare al costruttore
 $memento->title = "Memento";
 $memento->country = "United States";
+
+var_dump($trainspotting);
+var_dump($memento);
